7f8ae0b7be13f44bf4dce74c39c86fa2c27a7619
pikann <hvhai22@gmail.com>
2024-11-01 17:54:29 +0700

    chore: update registration API validation

--- project_name/users/serializers.py
+++ project_name/users/serializers.py
@@ -4,8 +4,8 @@ from project_name.users.models import User
 
 
 class RegistrationSerializer(serializers.ModelSerializer):
-    email = serializers.EmailField(max_length=50, min_length=6)
-    password = serializers.CharField(max_length=150, write_only=True)
+    email = serializers.EmailField()
+    password = serializers.CharField(max_length=150, write_only=True, min_length=8)
 
     class Meta:
         model = User
@@ -17,6 +17,25 @@ class RegistrationSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError({"email": "email already exists"})
 
         return super().validate(args)
+
+    def validate_password(self, value: str):
+        if not any(char.isdigit() for char in value):
+            raise serializers.ValidationError(
+                "This field must contain at least 1 digit."
+            )
+        if not any(char.isalnum() for char in value):
+            raise serializers.ValidationError(
+                "This field must contain at least 1 special letter"
+            )
+        if not any(char.isupper() for char in value):
+            raise serializers.ValidationError(
+                "This field must contain at least 1 upper case letter"
+            )
+        if not any(char.islower() for char in value):
+            raise serializers.ValidationError(
+                "This field must contain at least 1 lower case letter"
+            )
+        return value
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
